0564f135f631ee3c4dc1f0f7daede7508bd075c4
palomilu <meisen87@gmail.com>
2024-01-04 07:05:22 +0100

    progress morning 04012024

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,73 +1,48 @@
 import * as React from "react";
 
-// First component:
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
-  const array = ["Luis", "Julia", "Lukas"];
-
-  const welcome = {
-    title: "React",
-    greeting: "Hey",
-    family: array,
-  };
-
-  const bool = true;
-
-  const number = 123.45;
-
-  let notAssigned;
-
-  const empty = null;
-
-  const symbol = Symbol("Luis");
-
-  const listNames = welcome.family.map((name) => <li key={name.id}>{name}</li>);
-
   return (
     <div>
-      <h1>
-        {welcome.greeting}
-        {welcome.title}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
 
-      <h2>{`Hello again, let me introduce some primitive data types:`}</h2>
+      <hr />
 
-      <h3>Here, first, a list:</h3>
+      <h2>Lists in React</h2>
 
       <ul>
-        <li>{`A boolean variable declared in code has the value: ${bool}`}</li>
-        <li>{`A number variable declared is: ${number}`}</li>
-        <li>{`When something is not initialized// declared looks like this: ${notAssigned}`}</li>
-        <li>{`When a variable explicit has nothing: ${empty}`}</li>
-        <li>{`This is a kind of strange data type called symbol: ${typeof symbol}`}</li>
-        <li>{`... and finally an array from an object called and shown: ${welcome.family}`}</li>
+        {list.map((topic) => (
+          <li key={topic.objectID}>
+            <span>
+              <a href={topic.url}>{topic.title}</a>
+            </span>
+            <span>{topic.author}</span>
+            <span>{topic.num_comments}</span>
+            <span>{topic.points}</span>
+          </li>
+        ))}
       </ul>
-
-      <h2>
-        Now I will proceed to print elements of an array using the built-in map
-        function
-      </h2>
-
-      <h3>{`Let's show the elements like a list again`}</h3>
-      <ul>{listNames}</ul>
-
-      {/*Here using code from https://react.dev/learn/describing-the-ui */}
-      <br></br>
-      <h3>
-        Here I will put some code from the React Tutorial of the react.dev
-      </h3>
-      <section>
-        <h1>Amazing scientists</h1>
-        <Profile />
-        <Profile />
-        <Profile />
-      </section>
     </div>
   );
 }
---
+++ hacker-stories/src/playground.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4];
+
+const exponentialNumbers = numbers.map(function (number) {
+  return number * number;
+});
+
+console.log(exponentialNumbers);
